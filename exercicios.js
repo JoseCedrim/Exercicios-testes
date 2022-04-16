89c5f2ab184226af8544f919e63329ed1e7bae9b
JoseCedrim <jose.cedrim@hotmail.com>
2022-04-16 18:13:52 -0300

    Testando .reduce

--- exercicios.js
+++ exercicios.js
@@ -43,13 +43,32 @@
 
      //exercicio 3 Crie uma função que recebe uma lista de números e retorne somente os números positivos dessa lista. Se não tiver nenhum número positivo no array, retorne a string ‘Não há numeros positivos no array
 
-let numeroPositivos = [1, -3, 5, 6, -8, -9, 2, 4, -7, 3]
-function pickPositivos(array) {
-  let positivos = array.filter((numero)=>numero > 0)
-    if (positivos.length === 0){
-      return "Nao ha numeros positivos"
-    }
-      return positivos
-}   
-      console.log(pickPositivos(numeroPositivos))
+// let numeroPositivos = [1, -3, 5, 6, -8, -9, 2, 4, -7, 3]
+// function pickPositivos(array) {
+//   let positivos = array.filter((numero)=>numero > 0)
+//     if (positivos.length === 0){
+//       return "Nao ha numeros positivos"
+//     }
+//       return positivos
+// }   
+//       console.log(pickPositivos(numeroPositivos))
  
+    
+     //testando  .reduce
+
+ var numeros = [1,2,3,4,6,5,4]
+ var total = numeros.reduce(function(total,numeros){
+   return total + numeros; 
+ }); 
+ console.log(total);
+
+
+ var numeros1 = [25, -10, 45, 30, -25]
+ var total = numeros1.reduce(function(total, numeros1){
+   return total + numeros1
+ })
+ console.log(total)
+
+ var numeros2 = [25, -10, 45, 30, -25, 80, -5]
+ var total = numeros2.reduce((total, numeros2) => total + numeros2)
+ console.log(total)
